d61e9e366ce4b2595cbb26834b3e31cb82d23825
redlemon8 <hanquezbenjamin@gmx.com>
2024-12-18 13:21:39 +0100

    add async function

--- script.js
+++ script.js
@@ -3,20 +3,51 @@ started.addEventListener("click", startGame);
 const game = document.getElementById("game").style.display = "none";
 KeyboardEvent: key='Enter';
 
-let max = 50;
-let again = true;
+
+console.log('before startGame function');
+
+function startGame() {
+    document.getElementById("new-game").style.display = "none";
+    document.getElementById("game").style.display = "block";
+    console.log('cliked');
+}
+
+    async function play() {
+
+            const max = 500;
+            let nombreMystere = Math.floor(Math.random() * (max + 1));
+            let counter = 0;
+            let response = document.getElementById("response");
+            console.log('fonction async');
+            console.log(nombreMystere);
+            function game () {
+                if (parseInt(response) < nombreMystere) {
+                    counter++;
+                    alert('C\'est plus !')
+                } else if (parseInt(response) > nombreMystere){
+                    counter++; 
+                    alert('C\'est moins !')
+                } else {
+                    counter++;
+                    alert(`Félicitation vous avez trouvé le nombre mystère ! en ${counter} fois`);
+                }    
+            }
+        
+                while(parseInt(response) != nombreMystere) {
+                    response = await lireValeur();
+                    game();
+                }
+    }; play();
+        
 
 let value = null;
 
 async function lireValeur() {
   return new Promise(resolve => {
-
-    addForm();
     
     const idInterval = setInterval(() => {
       if (value !== null) {
         clearInterval(idInterval);
-        removeForm();
 
         // pour avoir le temps de voir le formulaire disparaître avant l'affichage d'un éventuel alert
         setTimeout(() => {
@@ -27,43 +58,4 @@ async function lireValeur() {
     }, 500);
 
   });
-}
-
-console.log('before startGame function');
-
-function startGame() {
-    document.getElementById("new-game").style.display = "none";
-    document.getElementById("game").style.display = "block";
-    console.log('cliked');
-}
-
-function findTheNumber() {
-    console.log('we have to find the number');
-    /* START THE LOOP */
-    while (again === true) {
-    console.log('are we here ?');
-                /* GENERATE A RANDOM NUMBER */
-        function getRandomInt(max) {
-
-            
-            return Math.floor(Math.random() * max);
-        }
-        
-        console.log(`le nombre mystère est : ${getRandomInt(max)}`);
-        
-        let numberPlayer = document.getElementById("number-player");
-
-        while (numberPlayer != getRandomInt(max)) {
-            if (numberPlayer < getRandomInt(max)) {
-                document.getElementsByClassName('title').textContent = `C/'est plus !`;
-                console.log("c/'est plus !");
-            } else if (numberPlayer > getRandomInt(max)) {
-                document.getElementsByClassName('title').textContent = `C/'est moins !`;
-                console.log("c/'est moins !");
-            } else {
-                document.getElementsByClassName('title').textContent = `Bravo vous avez trouvez le nombre mystère !`;
-                console ('vous avez trouvez le nombre mystère !');
-            }
-        }
-    }
 }
