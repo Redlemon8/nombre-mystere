ff3155e6789f2bd5ded9332dbfc0e30b0f41bee9
redlemon8 <hanquezbenjamin@gmx.com>
2025-01-27 15:53:08 +0100

    config: eslint install

--- check.js
+++ check.js
@@ -2,35 +2,26 @@ import { form } from "./form.js";
 
 export const check = {
 
-    // Génération du nombre mystère
-    game: {
-      nombreMystere: Math.floor(Math.random() * (500 + 1)),
-      counter: 0,
-    },
-
     init: function() {
       console.log('check.js started');
-      check.game.nombreMystere;
-        // check.play();
-        // check.lireValeur();
         // check.gameFunction();
          form.getNumber();
     },
+     gameFunction: function() {
+       if (parseInt(response) < check.game.nombreMystere) {
+         const tall = `Le nombre secret est plus grand que ${response}`;
+         displayClue(tall);                                                                  // FONCTION WITH GAME CONDITION
+       } else if (parseInt(response) > check.game.nombreMystere){
+           const little = `Le nombre secret est plus petit que ${response}\n`;
+           displayClue(little);
+       } else {
+           const finish = `Félicitation vous avez trouvé le nombre mystère ${check.game.nombreMystere} ! en ${check.game.counter++} fois`;
+           displayClue(finish);
+           removeForm();
+           transitionForm();
+       }
+   },
   }
-//     gameFunction: function() {
-//       if (parseInt(response) < check.game.nombreMystere) {
-//           const tall = `Le nombre secret est plus grand que ${response}`;
-//           displayClue(tall);                                                                  // FONCTION WITH GAME CONDITION
-//       } else if (parseInt(response) > check.game.nombreMystere){
-//           const little = `Le nombre secret est plus petit que ${response}\n`;
-//           displayClue(little);
-//       } else {
-//           const finish = `Félicitation vous avez trouvé le nombre mystère ${check.game.nombreMystere} ! en ${check.game.counter++} fois`;
-//           displayClue(finish);
-//           removeForm();
-//           transitionForm();
-//       }
-//   },
 
 //    // Vérification de la réponse est du nombre mystère
 //    play: async function () {
--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ import { check }  from "./check.js";
 const router = express.Router();
 
 router.use((req, res, next) => {
-    let mysteryNumber = Math.floor(Math.random() * 500) + 1; // Génère un nombre entre 1 et 100
+    const mysteryNumber = Math.floor(Math.random() * 500) + 1; // Génère un nombre entre 1 et 100
     console.log(mysteryNumber);
     next();
 });
